37a8618d6d7a01d4072e0e3e27e76d74ce32df64
Alfatih0901 <suckitarief@gmmail.com>
2021-05-24 14:50:33 +0700

    Tahap 1 Belajar Routing & Controller

---
+++ app/controllers/About.php
@@ -0,0 +1,12 @@
+<?php 
+
+class About {
+ public function index($nama = 'Arif', $pekerjaan='Sales')
+ {
+  echo "hallo nama saya $nama, saya adalah $pekerjaan";
+ }
+public function page()
+{
+   echo 'About/page';
+ }
+}
