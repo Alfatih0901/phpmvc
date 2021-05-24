3ac8ba730e7d225b7e425f5af6171457ecbc8029
Alfatih0901 <suckitarief@gmmail.com>
2021-05-24 16:40:39 +0700

    Menyesuaikan Assets css

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,5 @@
-
+<div class="container">
      <h1>About Me</h1>
+     <img src="<?= BASEURL; ?>/img/profile.jpg" alt="ARIF" width="200" class="rounded-circle shadow">
      <P>Haloo nama saya <?= $data['nama']; ?>, Umur Saya <?= $data['umur']; ?> tahun, Pekerjaan Saya <?= $data['pekerjaan']; ?></P>      
+</div>
