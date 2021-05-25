e97bb29c888c0ef137d3504d2a55910651b5d0cf
Alfatih0901 <suckitarief@gmmail.com>
2021-05-25 15:53:35 +0700

    Menambahkan Perintah Insert

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -1,6 +1,14 @@
-<div class="container mt-5">
+<div class="container mt-3">
+
            <div class="row">
                       <div class="col-6">
+                                 <!-- Button trigger modal -->
+                                 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#formModal">
+                                            Tambah Data Mahasiswa
+                                 </button>
+
+                                 <br><br>
+
                                  <h3>Daftar Mahasiswa</h3>
                                  <ul class="list-group">
                                             <?php foreach ($data['mhs'] as $mhs) : ?>
@@ -11,6 +19,55 @@
                                             <?php endforeach; ?>
                                  </ul>
 
+                      </div>
+           </div>
+
+</div>
+
+
+<!-- Modal -->
+<div class="modal fade" id="formModal" tabindex="-1" aria-labelledby="formModal" aria-hidden="true">
+           <div class="modal-dialog">
+                      <div class="modal-content">
+                                 <div class="modal-header">
+                                            <h5 class="modal-title" id="formModal">Form Tambah Data</h5>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                 </div>
+                                 <div class="modal-body">
+
+                                            <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="post">
+                                                       <div class="form-group">
+                                                                  <label for="nama">Nama</label>
+                                                                  <input type="text" class="form-control" id="nama" name="nama">
+
+                                                       </div>
+                                                       <div class="form-group">
+                                                                  <label for="nrp">Nrp</label>
+                                                                  <input type="number" class="form-control" id="nrp" name="nrp">
+
+                                                       </div>
+                                                       <div class="form-group">
+                                                                  <label for="email">Email</label>
+                                                                  <input type="email" class="form-control" id="email" name="email">
+
+                                                       </div>
+                                                       <div class="form-group">
+                                                                  <label for="jurusan">Jurusan</label>
+                                                                  <select class="form-control" id="jurusan" name="jurusan">
+                                                                             <option value="Teknik Informatika">Teknik Informatika</option>
+                                                                             <option value="Teknik Sipil">Teknik Sipil</option>
+                                                                             <option value="Teknik Industri">Teknik Industri</option>
+                                                                             <option value="Teknik Pangan">Teknik Pangan</option>
+                                                                             <option value="Teknik Mesin">Teknik Mesin</option>
+
+                                                                  </select>
+
+                                                                  <div class="modal-footer">
+                                                                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                                                             <button type="submit" class="btn btn-primary">Tambah Data</button>
+                                                                  </div>
+                                            </form>
+                                 </div>
                       </div>
            </div>
 </div>
--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,5 +1,10 @@
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-<script src="<?= BASEURL; ?>/js/bootstrap.js" ></script>
+<!-- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" crossorigin="anonymous"></script>
+<script src="http://localhost/phpmvc/public/js/bootstrap.js"></script> -->
+
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
 </body>
+
 </html>
