d467781d6827d49dbfdb6e064a20de3ef29f0250
Alfatih0901 <suckitarief@gmmail.com>
2021-05-25 14:34:43 +0700

    koneksi ke database

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -3,52 +3,24 @@
 
 class Mahasiswa_model
 {
-           // ngambil dari array
-           // private $mhs = [
-           //            [
-           //                       "nama" => "Alika Khairana",
-           //                       "nrp" => "01234556",
-           //                       "email" => "alika@gmail.com",
-           //                       "jurusan" => "Teknik Informatika"
-           //            ],
-           //            [
-           //                       "nama" => "Alfatih Khairan",
-           //                       "nrp" => "01234556",
-           //                       "email" => "alika@gmail.com",
-           //                       "jurusan" => "Teknik Informatika"
-           //            ],
-           //            [
-           //                       "nama" => "Sri Yulia",
-           //                       "nrp" => "01234556",
-           //                       "email" => "alika@gmail.com",
-           //                       "jurusan" => "Teknik Informatika"
-           //            ]
-
-
-
-           // ];
-
-
-           // Ngambil dari database
-           private $dbh; // database handler
-           private $stmt; // statement
+           private $table = 'mahasiswa';
+           private $db;
 
            public function __construct()
            {
-                      // datasource name
-                      $dsn = 'mysql:host=localhost;dbname=phpmvc';
-                      try {
-                                 $this->dbh = new PDO($dsn, 'root', '');
-                      } catch (PDOException $e) {
-                                 die($e->getMessage());
-                      }
+                      $this->db = new Database;
            }
 
            public function getAllMahasiswa()
            {
-                      // return $this->mhs; kalau pakai array
-                      $this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
-                      $this->stmt->execute();
-                      return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+                      $this->db->query('SELECT * FROM ' . $this->table);
+                      return $this->db->resultSet();
+           }
+
+           public function getMahasiswaByid($id)
+           {
+                      $this->db->query('SELECT * FROM ' . $this->table . ' WHERE id=:id');
+                      $this->db->bind('id', $id);
+                      return $this->db->single();
            }
 }
